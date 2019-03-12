edad6c2057269518678041b6386c54db568ee973
Park, Yunha <hadbsgk@gmail.com>
2019-03-12 21:57:29 +0900

    Add: Login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'decks', loadChildren: './decks/decks.module#DecksPageModule' }
+  { path: 'decks', loadChildren: './decks/decks.module#DecksPageModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule' }
 ];
 @NgModule({
   imports: [
