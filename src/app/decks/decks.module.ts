e529883faecc3088877cdaba8cb696a9418fd379
Park, Yunha <hadbsgk@gmail.com>
2019-03-17 14:15:13 +0900

    Integrate: Decks page

--- src/app/decks/decks.module.ts
+++ src/app/decks/decks.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
 import { DecksPage } from './decks.page';
 import { AuthGuard } from '../guards/auth.guard';
+import { DeckFormComponent } from './deck-form/deck-form.component';
 
 const routes: Routes = [
   {
@@ -21,8 +22,10 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
+    ReactiveFormsModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [DecksPage]
+  declarations: [DecksPage, DeckFormComponent],
+  entryComponents: [DeckFormComponent]
 })
 export class DecksPageModule {}
--- src/app/decks/decks.page.ts
+++ src/app/decks/decks.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DbService } from '../services/db.service';
+import { AuthService } from '../services/auth.service';
+import { ModalController } from '@ionic/angular';
+import { switchMap, shareReplay } from 'rxjs/operators';
+import { DeckFormComponent } from './deck-form/deck-form.component';
 
 @Component({
   selector: 'app-decks',
@@ -7,9 +13,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DecksPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    public router: Router
+    , public db: DbService
+    , public auth: AuthService
+    , public modal: ModalController
+  ) { }
+
+  decks;
 
   ngOnInit() {
+    this.decks = this.auth.user$.pipe(
+      switchMap(user =>
+        this.db.collection$('decks', ref =>
+          ref
+            .where('uid', '==', user.uid)
+            .orderBy('createdDate', 'desc')
+        )
+      ),
+      shareReplay(1)
+    );
+  }
+
+  // delete
+  deleteDeck(deck: any) {
+    this.db.delete(`decks/${deck.id}`);
+  }
+
+  // create or modify
+  async presentDeckForm(deck?: any) {
+    const modal = await this.modal.create({
+      component: DeckFormComponent,
+      componentProps: { deck }
+    });
+
+    return await modal.present();
+  }
+
+  trackById(idx, deck) {
+    return deck.id;
+  }
+
+  moveToDeckDetail(deck: any) {
+    // TODO: 절대경로가 아닌 상대경로로 설정해야하지 않을까?
+    this.router.navigate(['tabs', 'decks', deck.id]);
   }
 
 }
